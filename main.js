88b2a30105f3db099448b8d565e3ebf64bf18f74
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-08-06 16:37:06 -0500

    added dot files and moved event handling functions into component

--- main.js
+++ main.js
@@ -3,42 +3,40 @@ import ReactDOM from 'react-dom';
 import reduxLite from './redux-lite.js';
 import './demo.css';
 
-class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {number: 0};
-  }
-
-  render() {
-    return <div>
-      <div>
-        <button onClick={decrement}>-</button>
-        {this.state.number}
-        <button onClick={increment}>+</button>
-      </div>
-      <button onClick={reset}>Reset</button>
-    </div>;
-  }
-}
-
 const reducers = {
   decrement: state => ({number: state.number - 1}),
   increment: state => ({number: state.number + 1}),
   reset: () => ({number: 0})
 };
 
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {number: 0};
+  }
+  decrement() {
+    dispatch(reducers.decrement);
+  }
+  increment() {
+    dispatch(reducers.increment);
+  }
+  reset() {
+    dispatch(reducers.reset);
+  }
+  render() {
+    return <div>
+      <div>
+        <button onClick={this.decrement}>-</button>
+        {this.state.number}
+        <button onClick={this.increment}>+</button>
+      </div>
+      <button onClick={this.reset}>Reset</button>
+    </div>;
+  }
+}
+
 const topComponent = ReactDOM.render(
   <Counter/>,
   document.getElementById('content'));
 
 const dispatch = reduxLite(topComponent);
-
-function decrement() {
-  dispatch(reducers.decrement);
-}
-function increment() {
-  dispatch(reducers.increment);
-}
-function reset() {
-  dispatch(reducers.reset);
-}
