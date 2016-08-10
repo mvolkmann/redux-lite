3ca7b42c6d176854a7d4b9a7b090dae813e55531
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-08-09 21:20:59 -0500

    minor code formatting

--- redux-lite.js
+++ redux-lite.js
@@ -1,4 +1,4 @@
 // See README.md then demo/demo.js for an example of using this.
-export default topComponent => window.dispatch =
-  (reducerFn, payload, error, meta) =>
+export default topComponent =>
+  window.dispatch = (reducerFn, payload, error, meta) =>
     topComponent.setState(reducerFn(topComponent.state, payload, error, meta));
