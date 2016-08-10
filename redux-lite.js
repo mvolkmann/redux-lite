00f7560a596faf5d5ac4778f0a73da37b8628a4f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-08-09 21:18:29 -0500

    changed comments in redux-lite.js

--- redux-lite.js
+++ redux-lite.js
@@ -1,8 +1,4 @@
-// This creates a global function named "dispatch".
-// Event handling functions should call this
-// to modify the state held by the top component.
-// See README.md then main.js for an example of using this.
-export default topComponent => {
-  window.dispatch = (reducerFn, payload, error, meta) =>
+// See README.md then demo/demo.js for an example of using this.
+export default topComponent => window.dispatch =
+  (reducerFn, payload, error, meta) =>
     topComponent.setState(reducerFn(topComponent.state, payload, error, meta));
-};
